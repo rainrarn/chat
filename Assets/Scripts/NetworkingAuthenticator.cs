e8c7dfac68e94f058463f620e50c072614c48c1f
Kimhyuntae <tie314@naver.com>
2024-06-03 01:10:17 +0900

    네트워크 인증자 인증전용 네트워크 메시지 추가 및 클라전용 이벤트 추가

--- Assets/Scripts/NetworkingAuthenticator.cs
+++ Assets/Scripts/NetworkingAuthenticator.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 using Mirror;
 public class NetworkingAuthenticator : NetworkAuthenticator
 {
-#region Sereverside
+    public struct AuthReqMsg : NetworkMessage
+    {
+        // 인증을 위해 사용
+        // OAuth 같은 걸 사용 시 이 부분에 엑세스 토큰 같은 변수를 추가하면 됨
+        public string authUserName;
+    }
+
+    public struct AuthResMsg : NetworkMessage
+    {
+        public byte code;
+        public string message;
+    }
+
+    #region Sereverside
     [UnityEngine.RuntimeInitializeOnLoadMethod]
     static void ResetStatics()
     {
@@ -12,17 +25,20 @@ public class NetworkingAuthenticator : NetworkAuthenticator
     }
     public override void OnStartServer()
     {
+        //클라로부터 인증 요청 처리를 위한 핸들러 연결
+        NetworkServer.RegisterHandler<AuthReqMsg>(OnAuthRequestMessage, false);
     }
 
     public override void OnStopServer()
     {
+        NetworkServer.UnregisterHandler<AuthReqMsg>();
     }
 
     public override void OnServerAuthenticate(NetworkConnectionToClient conn)
     {
     }
 
-    public void OnAuthRequestMessage(NetworkConnectionToClient conn)
+    public void OnAuthRequestMessage(NetworkConnectionToClient conn, AuthReqMsg msg)
     {
     }
 
