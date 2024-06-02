1250bfe64fe007ecacd6278d56702d16d60265f8
Kimhyuntae <tie314@naver.com>
2024-06-02 23:38:05 +0900

    로그인팝업 네트워트 주소 및 유저이름 변경처리

--- Assets/Scripts/LoginPopup.cs
+++ Assets/Scripts/LoginPopup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Mirror;
 public class LoginPopup : MonoBehaviour
 {
     [Header("UI")]
@@ -21,10 +22,63 @@ public class LoginPopup : MonoBehaviour
 
     public static LoginPopup instance { get; private set; }
 
+    private string _originNetworkAddress;
 
     private void Awake()
     {
         instance = this;
     }
+
+    private void Start()
+    {
+        SetDefaultNetworkAddress();
+    }
+
+    private void OnEnable()
+    {
+        Input_UserName.onValueChanged.AddListener(OnValueChanged_ToggleButton);
+    }
+
+    private void OnDisable()
+    {
+        Input_UserName.onValueChanged.RemoveListener(OnValueChanged_ToggleButton);
+    }
+    private void Update()
+    {
+        CheckNetworkAddressNaledOnUpdate();
+    }
+
+    private void SetDefaultNetworkAddress()
+    {
+        //네트워크 주소 없는경우, 디폴트 세팅
+        if(string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        {
+            NetworkManager.singleton.networkAddress = "localhost";
+        }
+
+        //네트워크 주소 공란으로 변경될 경우대비 기존 네트워크 주소 보관
+        _originNetworkAddress = NetworkManager.singleton.networkAddress;
+    }
+
+    private void CheckNetworkAddressNaledOnUpdate()
+    {
+        if(string.IsNullOrWhiteSpace(NetworkManager.singleton.networkAddress))
+        {
+            NetworkManager.singleton.networkAddress = _originNetworkAddress;
+        }
+
+        if(Input_NetworkAddress.text != NetworkManager.singleton.networkAddress)
+        {
+            Input_NetworkAddress.text = NetworkManager.singleton.networkAddress;
+        }
+    }
+
+    public void OnValueChanged_ToggleButton(string userName)
+    {
+        bool isUserNameValid = !string.IsNullOrWhiteSpace(userName);
+        Btn_StartAsHostServer.interactable = isUserNameValid;
+        Btn_StartAsClient.interactable = isUserNameValid;
+    }
+
 
 }
