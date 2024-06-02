aa792c2def2af7c137e4f8e17432184202808d0a
Kimhyuntae <tie314@naver.com>
2024-06-03 00:05:14 +0900

    채팅UI 플레이어 정보 관리 컨테이너 추가, 채팅 목록 초기화

--- Assets/Scripts/ChattingUI.cs
+++ Assets/Scripts/ChattingUI.cs
@@ -14,14 +14,17 @@ public class ChattingUI : NetworkBehaviour
     [SerializeField] TMP_InputField Input_ChatMsg;
     [SerializeField] Button Btn_Send;
 
+
+    //서버 온리 - 연결된 플레이어들 이름
+    internal static readonly Dictionary<NetworkConnectionToClient, string> _connectedNameDic = new Dictionary<NetworkConnectionToClient, string>();
     public override void OnStartServer()
     {
-
+        _connectedNameDic.Clear();
     }
 
     public override void OnStartClient()
     {
-        
+        Text_ChatHistory.text = string.Empty;
     }
 
 
