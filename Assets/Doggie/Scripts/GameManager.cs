5fd9c39fe67ba8b341e612bb466848de3da76969
Kimhyuntae <tie314@naver.com>
2024-06-10 04:53:18 +0900

    준비버튼 누를 시 자동 시작함수

--- Assets/Doggie/Scripts/GameManager.cs
+++ Assets/Doggie/Scripts/GameManager.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance;
+
+
+    public Text countdownText; // 카운트다운 텍스트를 UI에 추가
+    private Coroutine countdownCoroutine;
 
     private void Awake()
     {
@@ -25,13 +30,48 @@ public class GameManager : NetworkBehaviour
             // 아직 준비되지 않은 플레이어가 있음
             if (!player.isReady)
             {
+                if (countdownCoroutine != null)
+                {
+                    StopCoroutine(countdownCoroutine);
+                    countdownCoroutine = null;
+                    RpcStopCountdown();
+                }
                 return;
             }
+        }
+        if (countdownCoroutine == null)
+        {
+            countdownCoroutine = StartCoroutine(StartCountdown());
+        }
+    }
+
+
+    private IEnumerator StartCountdown()
+    {
+        int countdown = 5;
+        while (countdown > 0)
+        {
+            RpcUpdateCountdown(countdown);
+            yield return new WaitForSeconds(1);
+            countdown--;
         }
 
         RpcStartGame();
     }
 
+
+    [ClientRpc]
+    private void RpcUpdateCountdown(int countdown)
+    {
+        countdownText.text = countdown.ToString();
+        countdownText.gameObject.SetActive(true);
+    }
+
+    [ClientRpc]
+    private void RpcStopCountdown()
+    {
+        countdownText.gameObject.SetActive(false);
+    }
     [ClientRpc]
     private void RpcStartGame()
     {
--- Assets/Doggie/Scripts/UIManager.cs
+++ Assets/Doggie/Scripts/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Button readyButton;
+    public Text countdownText;
     private NetPlayerObject localPlayer;
     private bool isReady = false; // 플레이어가 준비 상태인지 여부
 
@@ -21,7 +22,7 @@ public class UIManager : MonoBehaviour
         if (localPlayer != null)
         {
             isReady = !isReady;
-            localPlayer.CmdSetReady(true); // 플레이어가 준비 상태임을 서버에 알림
+            //localPlayer.CmdSetReady(true); // 플레이어가 준비 상태임을 서버에 알림
 
             // 버튼 색상 변경
             ColorBlock colors = readyButton.colors;
@@ -29,11 +30,13 @@ public class UIManager : MonoBehaviour
             {
                 colors.normalColor = Color.green; // 준비 상태일 때 색상
                 colors.selectedColor = Color.green;
+                localPlayer.CmdSetReady(true);
             }
             else
             {
                 colors.normalColor = Color.white; // 준비 해제 상태일 때 색상
                 colors.selectedColor = Color.white;
+                localPlayer.CmdSetReady(false);
             }
             readyButton.colors = colors;
         }
